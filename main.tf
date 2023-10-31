1d843e5f47df8cb5da8ec57a1349da7435c14767
sanyo123 <Ayibiowu123@>
2023-11-01 03:23:24 +0500

    adding code

---
+++ main.tf
@@ -0,0 +1,16 @@
+ module "aws_eks_cluster" {
+  source = "./module"
+ }
+
+ provider "aws" {
+  region = "us-west-2"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
